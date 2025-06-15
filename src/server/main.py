2b32aa34c52c0b7206c90f9f1b069e4eb6697b96
mhj <mohammadhj841019@gmail.com>
2025-06-15 11:51:52 +0330

    Port will be retrieved from command line args

--- src/server/main.py
+++ src/server/main.py
@@ -1,4 +1,15 @@
-import server_assign
+from server_assign import *
 
-new_server = server_assign.Server(5555,"127.0.0.1")
-new_server.bind_server()
+def main():
+    try:
+        if len(sys.argv) != 2:
+            raise ServerError(PORT_MISSING_EXCEPTION)
+        port = int(sys.argv[1])
+        ip = return_ip()
+        new_server = Server(port,ip)
+        new_server.bind_server()
+    except Exception as error:
+        print(f"{error}")
+
+if __name__ == "__main__":
+    main()
