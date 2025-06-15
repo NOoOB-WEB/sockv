e2a94eb6f8a368ebbc41e2ae2f751c1d4c949792
mhj <mohammadhj841019@gmail.com>
2025-06-15 11:54:11 +0330

    Ip will be automatically assigned

--- src/server/server_assign.py
+++ src/server/server_assign.py
@@ -1,14 +1,34 @@
 #importing socket and time which will be used in future
-#time is for controlling the server attempts 
+#time is for controlling the server attempts
+#sys for reading command-line arguments
 
 import socket
 import time
+import sys
 
 #binding exception when ip and port couldn't be assigned
-binding_exception = "Could not bind server to the desired ip and port !"
+BINDING_EXCEPTION = "Could not bind server to the desired ip and port !"
+#if ip cant be retrieved this error raise
+GETTING_IP_EXCEPTION = "Could not get ip address"
+#if user can not be retrieved this error raise
+GETTING_HOSTNAME_EXCEPTION = "Could not get ip address"
+#if user dont enter port or port can not be read, this error raise
+PORT_MISSING_EXCEPTION = "You didn't enter port!"
 
+#this class raised theexceptions i wrote
 class ServerError(Exception):
     pass
+
+#this function retrieves ip from system
+def return_ip():
+    try:
+        host = socket.gethostname()
+        ip = socket.gethostbyname(host)
+        print(f"ip found: \nIP:{ip}")
+        time.sleep(0.5)
+        return ip
+    except Exception:
+        raise ServerError()
 
 class Server:
     #port and ip will be given to bind the werver on it
@@ -24,6 +44,6 @@ class Server:
             self.socket_server.bind((self.ip,self.port))
             time.sleep(0.5)
         except Exception:
-            raise ServerError(binding_exception) 
+            raise ServerError(BINDING_EXCEPTION) 
         else:
             print(f"Server running on port:{self.port} and ip:{self.ip}")
